fbe6d6457c005af2f4b8cd79ba6c9a79c0078252
akshata.darakhe <akshata.darakhe@capslock.studio>
2024-09-24 15:01:38 +0530

    FDns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+ eslint:{
+       ignoreDurinBuilds: true,
+ },
+ }
+ module.export = nextConfig;
